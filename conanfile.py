7fef3d91fc881cec447e75458e2975fee99dce6f
BloodyProg <bloody.prog@gmail.com>
2016-06-22 22:55:30 -0400

    test premake-5.0.0-alpha9

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,33 @@
 import os
 from conans import ConanFile
+from conans.tools import download, unzip
 
 channel = os.getenv("CONAN_CHANNEL", "channel")
 username = os.getenv("CONAN_USERNAME", "username")
 
 class TestPackage(ConanFile):
+    name = "test-package"
+    version = "0.1"
     settings = "os"
 
     def build(self):
-        self.output.warn("BUILD...")
+        archive = "premake.zip"
+        download("https://github.com/premake/premake-core/releases/download/v5.0.0-alpha9/premake-5.0.0-alpha9-src.zip", archive)
+        unzip(archive)
+        os.unlink(archive)
+
+        if self.settings.os == "Windows":
+            os.chdir("premake-5.0.0-alpha9/build/gmake.windows")
+            self.run("nmake")
+            os.chdir("../..")
+
+        elif self.settings.os == "Macos":
+            os.chdir("premake-5.0.0-alpha9/build/gmake.macosx")
+            self.run("make")
+
+        elif self.settings.os == "Linux":
+            os.chdir("premake-5.0.0-alpha9/build/gmake.unix")
+            self.run("make")
 
     def package(self):
-        self.output.warn("PACKAGE...")
+        self.copy("*", dst="bin", src="premake-5.0.0-alpha9/bin/release")
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,8 +8,13 @@ class TestTestPackage(ConanFile):
     settings = "os"
     requires = "test-package/0.1@%s/%s" % (username, channel)
 
+    def imports(self):
+        self.output.warn("IMPORTS...")
+        self.copy("*", dst="bin", src="bin")
+
     def build(self):
         self.output.warn("BUILD...")
 
     def test(self):
         self.output.warn("TEST...")
+        self.run("ls")
