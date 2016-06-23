05f3fd961cf69b1ff170606130c72147703e1c1d
BloodyProg <bloody.prog@gmail.com>
2016-06-22 22:10:40 -0400

    script

--- travis_script.sh
+++ travis_script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo travis script
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+    eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+python build.py
